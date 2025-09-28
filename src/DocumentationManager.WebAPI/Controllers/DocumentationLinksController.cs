ce192b2c126087016a799cf6caa8230e91ad611b
d-luan <dluan1285@gmail.com>
2025-09-28 16:24:15 -0300

    feat: Add GET endpoints for documentation links

--- src/DocumentationManager.WebAPI/Controllers/DocumentationLinksController.cs
+++ src/DocumentationManager.WebAPI/Controllers/DocumentationLinksController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DocumentationManager.WebAPI.Model;
 using DocumentationManager.WebAPI.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DocumentationManager.WebAPI.Controllers;
 
@@ -14,5 +19,33 @@ public class DocumentationLinksController : ControllerBase
     public DocumentationLinksController(DocumentationContext context)
     {
         _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetDocumentationLinks()
+    {
+        try
+        {
+            var links = await _context.DocumentationLinks.ToListAsync();
+
+            return Ok(links);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Ocorreu um erro interno no servidor: {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<DocumentationLink>> GetDocumentationLinkById(int id)
+    {
+        var documentationLink = await _context.DocumentationLinks.FindAsync(id);
+
+        if(documentationLink == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(documentationLink);
     }
 }
